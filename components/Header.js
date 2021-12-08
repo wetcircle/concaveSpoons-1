c103151928151e97c0cb572bcc3ea18db8701b5f
NUnicorn2860 <95504158+NUnicorn2860@users.noreply.github.com>
2021-12-07 17:19:05 -0800

    Fix mobile view whitespace issue

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ function Header() {
         <header className="sticky text-sm md:text-lg top-0 z-50 grid grid-cols-1 md:grid-cols-2 gap-y-3 p-5 md:px-10 items-center bg-gray-medium2">
             {/* Left */}
             <div>
-                <div className="flex w-max border-2 justify-center items-center py-3 space-x-3 sm:space-x-5 px-6">
+                <div className="flex overflow-hidden border-2 justify-center items-center py-3 space-x-3 sm:space-x-5 px-6">
                     <div className="hover:animate-ping cursor-pointer font-bold">mint</div>
                     <div className="text-xl">∞</div>
                     <div className="hover:animate-ping cursor-pointer font-bold">IPFS spoons</div>
