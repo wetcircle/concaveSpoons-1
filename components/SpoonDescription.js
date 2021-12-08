7edd2349e9347370b26b9c309f42fd42f1a5408e
shanghigh <95733088+shanghigh@users.noreply.github.com>
2021-12-08 11:16:16 -0500

    Update SpoonDescription.js

--- components/SpoonDescription.js
+++ components/SpoonDescription.js
@@ -5,7 +5,7 @@ function SpoonDescription() {
     return (
         <div className="space-y-8">
             <h1 className="text-4xl font-extrabold">The Concave Spoons</h1>
-            <p className="text-lg">The Concave Spoons is a collection of 4317 images, exactly the same number of Colors NFTs in existence, living on IPFS. The backgrounds used in the collection are the Colors NFTs, in the same exact order. In a world of forks, frens spoon frens</p>
+            <p className="text-lg">The Concave Spoons is a collection of 4317 images, exactly the same number of Colors NFTs in existence, living on IPFS. The backgrounds used in the collection are the Colors NFTs, in the same exact order. In a world of forks, frens spoon frens!</p>
             <div className="space-y-3">
                 <div className="flex space-x-6 items-center text-2xl">
                     <a href="https://twitter.com/Concave_Spoons" className="hover:animate-spin cursor-pointer"><BsTwitter /></a>
@@ -14,9 +14,9 @@ function SpoonDescription() {
                 </div>
                 <div className="border-b w-90 pt-2" />
                 <div className="flex space-x-5 items-center">
-                    <div className="text-lg">Price = 0.03</div>
+                    <div className="text-lg">Price = 0.04</div>
                     <div className="text-xl">∞</div>
-                    <div className="text-lg"> Minted </div>
+                    <div className="text-lg"> Minted 0/4317 </div>
                 </div>
             </div>
         </div>
