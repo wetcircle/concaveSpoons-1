e14988d3db85621623086bbc2003194047da5d9c
NUnicorn2860 <95504158+NUnicorn2860@users.noreply.github.com>
2021-12-08 01:55:10 -0800

    Update "more" page

--- pages/more.js
+++ pages/more.js
@@ -1,13 +1,18 @@
 import Header from '../components/Header'
+import AboutDescription from '../components/AboutDescription'
 
 function more() {
     return (
         <div className="bg-gray-medium text-yellow-medium">
             <Header />
-            <main className="mt-20 max-w-7xl mx-auto px-8 md:px-16 h-800px md:h-[1100px] space-y-10 text-center items-center">
-            <p className="text-4xl">Potentially would be the showcase of IPFS spoon images.</p>
-            <p className="text-2xl">Grid view?</p>
-            <p className="text-2xl">Carousel?</p>
+            <main className="mt-20 max-w-7xl mx-auto px-8 md:px-16 h-800px md:h-[1100px]">
+            <section className="pt-6 mt-20 grid grid-cols-1 lg:grid-cols-2 md:space-x-10 gap-y-10">
+                    <div className="">
+                        <AboutDescription />
+                    </div>
+                    <div className="">
+                    </div>
+                </section>
             </main>
         </div>
     )
