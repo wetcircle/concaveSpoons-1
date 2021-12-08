3a448d352fa0c8b150490c48dd4c62b3cdbbd782
NUnicorn2860 <95504158+NUnicorn2860@users.noreply.github.com>
2021-12-08 15:40:57 -0800

    Fix overflow issues for specific screen size

--- components/Frame.js
+++ components/Frame.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import spoon from "../public/spoon.gif";
-import MintSlider from "./MintSlider";
 import { useState } from 'react';
 
 function Frame() {
@@ -8,8 +7,8 @@ function Frame() {
 
     return (
         <div className="-ml-2 md:ml-0 overflow-hidden flex-col">
-            <div className="h-[350px] w-[350px] md:h-[500px] md:w-[500px] border-2">
-                <div className="relative h-[335px] w-[335px] md:h-[480px] md:w-[480px] border-2 mx-auto mt-[8px] mb-[5px]">
+            <div className="h-[300px] w-[300px] md:h-[500px] md:w-[500px] border-2">
+                <div className="relative h-[290px] w-[290px] md:h-[480px] md:w-[480px] border-2 mx-auto mt-[3px] md:mt-[8px] mb-[5px]">
                     <Image src={spoon} layout="fill" objectFit="cover" />
                 </div>
                 {/* <div className="flex grid-cols-2">
