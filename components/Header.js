9180b19e5237c362b6bccacfb07aad1ee502ee97
wetcircle <65508703+wetcircle@users.noreply.github.com>
2021-12-08 11:24:48 -0500

    Metamask support test

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,13 @@ import { useRouter } from "next/dist/client/router";
 
 function Header() {
     const router = useRouter();
-
+    const ethereumButton = document.querySelector('.enableMetaMask');
+    ethereumButton.addEventListener('click', () => {
+        ethereum.request({ method: 'eth_requestAccounts' });
+    });
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+    console.log(account);
     return (
         <header className="sticky text-sm md:text-lg top-0 z-50 grid grid-cols-1 md:grid-cols-2 gap-y-3 p-5 md:px-10 items-center bg-gray-medium2">
             {/* Left */}
@@ -21,7 +27,7 @@ function Header() {
 
             {/* Right */}
             <div className="flex items-center space-x-4 justify-end">
-                <div className="hover:animate-bounce flex items-center space-x-2 border-2 py-3 px-5 cursor-pointer">
+                <div class="enableMetaMask" className="hover:animate-bounce flex items-center space-x-2 border-2 py-3 px-5 cursor-pointer">
                     <p>connect</p>
                     <FaWallet />
                 </div>
