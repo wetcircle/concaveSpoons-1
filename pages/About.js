38d6301b4eaea29ad26ccb41148722ebcf5fe6fb
zer0conf <25299353+zer0conf@users.noreply.github.com>
2021-12-10 16:15:10 +0200

    Update About.js
    Remove TeamTable

--- pages/About.js
+++ pages/About.js
@@ -1,6 +1,6 @@
 import Header from "../components/Header";
 import AboutDescription from "../components/AboutDescription";
-import TeamTable from "../components/TeamTable";
+//import TeamTable from "../components/TeamTable";
 
 function About() {
     return (
@@ -10,9 +10,6 @@ function About() {
                 <section className="pt-6 mt-20 grid grid-cols-1 lg:grid-cols-2 md:space-x-10 gap-y-10">
                     <div className="">
                         <AboutDescription />
-                    </div>
-                    <div className="">
-                        <TeamTable />
                     </div>
                 </section>
             </main>
