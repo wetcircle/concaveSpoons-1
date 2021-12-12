30fd6745f635f2029a020e4ca4a3609f41934a1d
zer0conf <25299353+zer0conf@users.noreply.github.com>
2021-12-12 05:11:19 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -87,6 +87,7 @@ export default function Home() {
   injected.isAuthorized().then((isAuthorized) => {
     if (isAuthorized) {
       setIsConnected(true);
+      showAlert(false);
     }
   });
  }, []);
