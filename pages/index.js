de0faa6d9602d77e7235b1bc63e319925714c817
zer0conf <25299353+zer0conf@users.noreply.github.com>
2021-12-13 16:47:48 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ function IPFSSplash() {
     return (
         <div className="bg-gray-medium text-yellow-medium">
             <main className="max-w-7xl mx-auto px-8 h-[800px] md:px-16 md:h-[1100px] space-y-10 text-center items-center">
-                <br>
+                <br></br>
                 <h1 className="text-3xl">The Concave Spoons NFT Collection.</h1>
                 <p className="text-2xl">Launching December 13th.</p>
                 <div className="ml-[200px] sm:ml-[400px] lg:ml-[550px] animate-pulse text-5xl"><GiSandsOfTime /></div>
